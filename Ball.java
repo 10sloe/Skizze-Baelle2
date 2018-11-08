8aa6d299702c6fe7e7e94ea26fa5c24d0e941c6c
SABINESURF\Sabine <sabsloe@gmail.com>
2018-11-08 09:03:02 +0100

    ohne public

--- Ball.java
+++ Ball.java
@@ -1,39 +1,38 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-public class Ball
+class Ball
 {
     // Attribute
     double x;
     double y;
     double v;
-    double a = 0.1;
     Color farbe;
     
-    // Konstruktor
-    public Ball(double x_, double y_, double v_, Color farbe_)
+    // Konstruktor    
+    Ball(double x_, double y_, double v_, Color farbe_)
     {
         x = x_;
         y = y_;
         v = v_;
         farbe = farbe_;
     }
-    
-    public void zeichnen(GraphicsContext gc)
+     
+    void bewegen()
     {
-        gc.setFill(farbe);    // Ball1 zeichnen
-        gc.fillOval(x,y,20,20); 
-    }
-    
-    public void bewegen()
-    {
-        v = v + a;     // Ball1 bewegen
+        v = v + 0.1;     
         y = y + v; 
         
-        if(y > 400)  // Ball1 wird am unteren Rand reflektiert
+        if(y > 400)  
         {
             v = -v;
         }
+    }
+    
+    void zeichnen(GraphicsContext gc)
+    {
+        gc.setFill(farbe);  
+        gc.fillOval(x,y,20,20); 
     }
     
 }
--- Skizze.java
+++ Skizze.java
@@ -1,34 +1,26 @@
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-class Skizze
+public class Skizze
 {
     // Es soll drei Bälle geben
-    Ball ball1;  
-    Ball ball2;
-    Ball ball3;   
+    Ball ball1 = new Ball(100, 150, 2, Color.RED);
+    Ball ball2 = new Ball(150, 100, 1, Color.BLUE);
+    Ball ball3 = new Ball(200, 100,-2, Color.GREEN);  
     
-    Skizze()
-    {
-        // Hier werden die drei Ballobjekte erzeugt
-        ball1 = new Ball(100,100,2, Color.RED); 
-        ball2 = new Ball(150,100,-2, Color.BLUE);
-        ball3 = new Ball(200,100,0, Color.GREEN);
-    }
- 
     // Diese Methode wird immer und immer wieder ausgeführt
     void draw(GraphicsContext gc){
-        gc.clearRect(0,0,400,400);
-        
-        // Die drei Bälle werden gezeichnet
-        ball1.zeichnen(gc);
-        ball2.zeichnen(gc);
-        ball3.zeichnen(gc);
-        
+   
         // Die drei Bälle werden bewegt
         ball1.bewegen();
         ball2.bewegen();
         ball3.bewegen();
+             
+        // Die drei Bälle werden gezeichnet
+        gc.clearRect(0,0,400,400);
+        ball1.zeichnen(gc);
+        ball2.zeichnen(gc);
+        ball3.zeichnen(gc);
         
     }
 
